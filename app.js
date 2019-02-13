7c4dc1bd5272cd959e692f29e20d2b253f893023
Brigg Lewis <d00256609@dmail.dixie.edu>
2019-02-13 13:35:18 -0700

    first commit

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
-var lunchPlaces = null;
+var First = document.querySelector("#first");
+var Last = document.querySelector("#last");
+var body = document.querySelector("body");
+var inputcolor = document.querySelector("input::placeholder")
 
-// let's make an event
-var theButton = document.querySelector("#the-button");
-console.log("the button is", theButton);
-theButton.onclick = function () {
-  // add an item to the list when the button is clicked
-  var theList = document.querySelector("#the-list");
-  console.log("the list is", theList);
-
-  var nameInput = document.querySelector("#name");
-  var lunchVenueInput = document.querySelector("#lunch-venue");
-  console.log("the input is", lunchVenueInput);
-
-  var newItem = document.createElement("li");
-  newItem.innerHTML = nameInput.value + " wants to eat at " + lunchVenueInput.value;
-  theList.appendChild(newItem);
+var theme = document.querySelector("#theme");
+theme.onclick = function () {
+    if (theme.innerHTML == "Dark Mode"){
+    body.style.background = "black"
+    body.style.color = "white"
+    theme.innerHTML = "Light Mode"
+    button.style.color = "white"
+    inputcolor.style.color = "white"
+    
+    }
+    else{
+    button.style.color = "black"
+    body.style.background = "white"
+    body.style.color = "black"
+    console.log("theme");
+    theme.innerHTML = "Dark Mode"
+    inputcolor.style.color = "black"
+    }
 };
 
-fetch("https://api.myjson.com/bins/z1lsc").then(function (response) {
-  response.json().then(function (data) {
-    // save all of the data into a global variable (to use later)
-    lunchPlaces = data;
+// let's make an event
+var theButton = document.querySelector("#button");
+theButton.onclick = function () {
+    console.log(First.value)
+    console.log(Last.value)
+    if (First != "" && Last != ""){
+  var data = "name=" + encodeURIComponent(First.value + Last.value);
 
-    // data is an array of string values
-    var suggestionsList = document.querySelector("#suggestions");
-
-    // add the restaurants to the suggestions list
-    data.forEach(function (restaurant) { // for restaurant in data
-      var newItem = document.createElement("li");
-      newItem.innerHTML = restaurant;
-      newItem.className = "restaurant";
-      suggestionsList.appendChild(newItem);
-    });
+  fetch("http://localhost:8080/Athletes", {
+    method: 'POST',
+    body: data,
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded"
+    }
+  }).then(function (response) {
+    console.log("student saved.");
+    getStuff()
   });
-});
+  }
+};
 
+function getStuff(){
+    fetch("http://localhost:8080/Athletes").then(function (response) {
+      response.json().then(function (data) {
+        // save all of the data into a global variable (to use later)
+        IDs = data;
+
+        // data is an array of string values
+        var idList = document.querySelector("#idList");
+
+        // add the restaurants to the suggestions list
+          idList.innerHTML = "";
+          data.forEach(function (id) { // for restaurant in data
+          var newItem = document.createElement("li");
+          newItem.innerHTML = id;
+          newItem.className = "idList";
+          idList.appendChild(newItem);
+        });
+      });
+    });
+}
+getStuff()
---
+++ server.py
@@ -0,0 +1,69 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+import json
+
+
+def readAthletes(x):
+    athleteList = []
+    f = open(x, "r")
+    for line in f:
+        line = line.strip()
+        athleteList.append(line)
+    f.close()
+    return athleteList
+
+def writeAthletes(listx, x):
+    athleteString = ""
+    f = open(x, "w")
+    for ID in listx:
+        athleteString += ID + "\n"
+    f.write(athleteString)
+    f.close()
+
+Athletes = readAthletes("athletes.txt")
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/Athletes":
+            self.send_response(200)
+            # all headers go here:
+            self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            #add stuff to read the list file
+            Athletes = readAthletes("athletes.txt")
+            self.wfile.write(bytes(json.dumps(Athletes), "utf-8"))
+        else:
+            self.send_response(404)
+            self.end_headers()
+        return
+
+    def do_POST(self):
+        if self.path == "/Athletes":
+            length = self.headers["Content-length"]
+            body = self.rfile.read(int(length)).decode("utf-8")
+            print("the text body:", body)
+            parsed_body = parse_qs(body)
+            print("the parsed body:", parsed_body)
+
+            Athletes.append(parsed_body["name"][0])
+            writeAthletes(Athletes, "athletes.txt")
+
+            self.send_response(201)
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+        return
+
+def run():
+    listen = ("127.0.0.1", 8080)
+    server = HTTPServer(listen, MyRequestHandler)
+
+    print("Listening...")
+    server.serve_forever()
+
+run()
